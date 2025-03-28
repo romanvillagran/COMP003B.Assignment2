6712815166046542afdd997f2df3db380f3a77aa
romanvillagran <romanvillagran386@gmail.com>
2025-03-28 00:15:07 -0700

    basically added the controller from the lecture assisment to here and also added each action like current, completed and ideas

---
+++ COMP003B.Assignment2/Controllers/ProjectsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment2.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Current()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Completed()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Ideas()
+        {
+            return View();
+        }
+    }
+}
