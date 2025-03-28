bc207b4d92a4ec01d8b5fa48f3611f48bc81fe8b
romanvillagran <romanvillagran386@gmail.com>
2025-03-27 23:57:33 -0700

    added the rest for the middleware

--- COMP003B.Assignment2/Middleware/RequestTrackerMiddleware.cs
+++ COMP003B.Assignment2/Middleware/RequestTrackerMiddleware.cs
@@ -10,5 +10,15 @@
             _next = next;
             _logger = logger;
         }
+
+        public async Task Invoke(HttpContext context)
+        {
+            //this Log the HTTP request method and path 
+            _logger.LogInformation("Request: {Method} {Path}", context.Request.Method, context.Request.Path);
+            await _next(context);
+            //while this procces the middleware request and then logs the http status code
+            _logger.LogInformation("Response: {StatusCode}", context.Response.StatusCode);
+        }
+
     }
 }
